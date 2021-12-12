d13d6fb51f008508f74d29dbf313ed613743a10b
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2021-12-12 17:48:22 +0300

    feat: package name changed & language codes added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('dev dev');
-  }
-  return a + b;
-};
+import translate from '@vitalets/google-translate-api';
+import { languages } from './languages';
+
+export function translateME(word: string, from: string, to: string) {
+  translate(word, { from: from, to: to }).then(res => {
+    console.log(res.text);
+  });
+}
+
+export function translateObject(
+  object: object,
+  from: languages,
+  to: languages
+) {
+  console.log(`translate this object: `, object);
+  console.log(`from: `, from);
+  console.log(`to: `, to);
+}
